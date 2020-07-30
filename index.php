cf5faa062a30e9c20bc76ea9b91b5d0f2e24b1da
Luciano Freitas <lucsilfreitas@gmail.com>
2020-07-30 18:39:08 -0300

    A105

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+
 
 
 $app = new \Slim\Slim();
@@ -13,9 +15,16 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 $page = new Page();
-$page = setTpl("index");
+$page -> setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+    
+    $page = new PageAdmin();
+    $page -> setTpl("index");
+    
+    });
 
 $app->run();
 
